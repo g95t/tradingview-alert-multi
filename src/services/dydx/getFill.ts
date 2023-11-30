dfe55f68577b117d316d76c061eb4b12987b1239
g95t <144741189+g95t@users.noreply.github.com>
2023-11-30 17:44:26 +0100

    Update getFill.ts

--- src/services/dydx/getFill.ts
+++ src/services/dydx/getFill.ts
@@ -1,13 +1,14 @@
 import DYDXConnector from './client';
 import { FillResponseObject } from '@dydxprotocol/v3-client';
 import { _sleep } from '../../helper';
+import { AlertObject } from '../../types';
 
-export const getFill = async (order_id: string) => {
+export const getFill = async (order_id: string, alertMessage: AlertObject) => {
 	let count = 0;
 	const maxTries = 3;
 	while (count <= maxTries) {
 		try {
-			const connector = await DYDXConnector.build();
+			const connector = await DYDXConnector.build(alertMessage);
 			const allFills: { fills: FillResponseObject[] } =
 				await connector.client.private.getFills({ orderId: order_id });
 
