1dc9d611e1a6921fd2515e06dfeaa94798c1c8c7
g95t <144741189+g95t@users.noreply.github.com>
2023-11-30 17:44:09 +0100

    Update getAccount.ts

--- src/services/dydx/getAccount.ts
+++ src/services/dydx/getAccount.ts
@@ -1,13 +1,40 @@
 import DYDXConnector from './client';
 import 'dotenv/config';
+import { AlertObject } from '../../types';
 
-export const dydxGetAccount = async () => {
+export const dydxGetAccount = async (alertMessage: AlertObject) => {
 	try {
-		const connector = await DYDXConnector.build();
+		const connector = await DYDXConnector.build(alertMessage);
 		if(!connector) return false;
 		
+		let ETHADDRESS: string | undefined;
+
+		switch (alertMessage.exchID) {
+			case 1:
+				ETHADDRESS = process.env.ETH_ADDRESS1!;
+				break;
+      			case 2:
+				ETHADDRESS = process.env.ETH_ADDRESS2!;
+				break;
+			case 3:
+				ETHADDRESS = process.env.ETH_ADDRESS3!;
+				break;
+      			case 4:
+				ETHADDRESS = process.env.ETH_ADDRESS4!;
+				break;
+			case 5:
+				ETHADDRESS = process.env.ETH_ADDRESS5!;
+				break;
+      			case 6:
+				ETHADDRESS = process.env.ETH_ADDRESS6!;
+				break;
+			default:
+				ETHADDRESS = process.env.ETH_ADDRESS!;
+				break;
+    		}
+		
 		const account = await connector.client.private.getAccount(
-			process.env.ETH_ADDRESS
+			ETHADDRESS
 		);
 		console.log('dYdX account: ', account);
 		if (Number(account.account.freeCollateral) == 0) {
