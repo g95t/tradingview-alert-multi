58528b69489334ceff55b53f7d8b4128dc9518a2
g95t <144741189+g95t@users.noreply.github.com>
2023-11-30 17:44:40 +0100

    Update getOrder.ts

--- src/services/dydx/getOrder.ts
+++ src/services/dydx/getOrder.ts
@@ -1,14 +1,15 @@
 import DYDXConnector from './client';
 import { OrderResponseObject } from '@dydxprotocol/v3-client';
 import { _sleep } from '../../helper';
+import { AlertObject } from '../../types';
 
-export const getOrder = async (order_id: string) => {
+export const getOrder = async (order_id: string, alertMessage: AlertObject) => {
 	let count = 0;
 	const maxTries = 3;
 	let filled;
 	while (count <= maxTries && !filled) {
 		try {
-			const connector = await DYDXConnector.build();
+			const connector = await DYDXConnector.build(alertMessage);
 			const orderResponse: { order: OrderResponseObject } =
 				await connector.client.private.getOrderById(order_id);
 
