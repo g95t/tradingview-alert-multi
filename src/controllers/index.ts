030b9b85f4850917c61ccdb9471c09ce3db68e7c
g95t <144741189+g95t@users.noreply.github.com>
2023-12-04 15:01:26 +0100

    Update index.ts

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -32,8 +32,20 @@ router.get('/', async (req, res) => {
 		res.send(message);
 	}
 
-  	const forwardedFor = req.headers['x-forwarded-for'];
-  	const clientIP = forwardedFor ? forwardedFor.split(',')[0] : req.ip;
+
+	const forwardedFor = req.headers['x-forwarded-for'];
+	let clientIP: string;
+	if (forwardedFor) {
+		if (typeof forwardedFor === 'string') {
+			clientIP = forwardedFor;
+		} else if (Array.isArray(forwardedFor)) {
+			clientIP = forwardedFor[0];
+		} else {
+			clientIP = req.ip;
+		}
+	} else {
+		clientIP = req.ip;
+	}
 	console.log('IP address:', clientIP);
 });
 
